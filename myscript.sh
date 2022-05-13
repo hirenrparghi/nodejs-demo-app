a1f695224724e9d19b0ae52f7a418789c652c612
Hiren Parghi <hirenparghi90@gmail.com>
2022-05-13 16:58:58 +0530

    4:58

---
+++ myscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Purpose: While loop Continue Statement
+#Version: 1.0
+#created Date: Wed Sep 29 07:24:04 UTC 2021
+#Modified Date:
+#Author: Hiren Parghi
+#START #
+
+echo asking the OS nameeeeeee?
+cat /etc/os-release
+
+
+#END #
