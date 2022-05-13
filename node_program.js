7f100b881095c9c65a9a423b4fdb2f6ebc82453c
Hiren Parghi <hirenparghi90@gmail.com>
2022-05-13 18:15:53 +0530

    6:15

--- node_program.js
+++ node_program.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+import { exec } from 'child_process';
 var yourscript = exec('sh myscript.sh',
         (error, stdout, stderr) => {
             console.log(stdout);
